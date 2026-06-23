0a38cb50e00dace59431c0512fe1aa1d51a994cf
Booker <nicolaspatino77@gmail.com>
2026-06-23 09:29:01 +0200

    :sparkles: exercice fonts images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Arimo } from "next/font/google"
+
+
+const arimo = Arimo({
+  weight: ["400", "600"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-arimo",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${arimo.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
